fb08f18495123056191cf1056632ec03fe10ef3b
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-06-01 11:23:34 +0200

    Naming routes

--- routes/api.php
+++ routes/api.php
@@ -21,48 +21,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/signup',[UserController::class,'register']);
-Route::post('/login',[UserController::class,'login']);
-Route::get('/professional_profiles/search',[ProfessionalProfileController::class,'search']);
-Route::get('/roles',[RoleController::class,'getAll']);
+Route::post('/signup',[UserController::class,'register'])->name('signup');
+Route::post('/login',[UserController::class,'login'])->name('login');
+Route::get('/professional_profiles/search',[ProfessionalProfileController::class,'search'])->name('professionalProfile.search');
+Route::get('/roles',[RoleController::class,'getAll'])->name('roles.getAll');
 
 //Protected routes
 Route::group(['middleware' => ['auth:api']], function() {
 
     // User - routes
     Route::group(['prefix'=>'/user'],function(){
-        Route::get('/info',[UserController::class,'info']);
-        Route::post('/logout',[UserController::class,'logout']);
+        Route::get('/info',[UserController::class,'info'])->name('user.info');
+        Route::post('/logout',[UserController::class,'logout'])->name('logout');
     });
 
     // Job - routes
     Route::group(['prefix'=>'/job'],function(){
-        Route::post('/',[JobController::class,'store']);
-        Route::get('/search',[JobController::class,'search']);
-        Route::get('/{id}',[JobController::class,'info']);
-        Route::put('/',[JobController::class,'update']);
-        Route::get('/additional_info/{id}/{scope?}',[JobController::class,'additionalInfo']);
-        Route::post('/update_active',[JobController::class,'updateActiveValue']);
+        Route::post('/',[JobController::class,'store'])->name('job.store');
+        Route::get('/search',[JobController::class,'search'])->name('job.search');
+        Route::get('/{id}',[JobController::class,'info'])->name('job.info');
+        Route::put('/',[JobController::class,'update'])->name('job.update');
+        Route::get('/additional_info/{id}/{scope?}',[JobController::class,'additionalInfo'])->name('job.additionalInfo');
+        Route::post('/update_active',[JobController::class,'updateActiveValue'])->name('job.updateActive');
     });
 
     //Candidature - routes
     Route::group(['prefix'=>'/candidature'],function(){
-        Route::post('/',[CandidatureController::class,'store']);
-        Route::get('/my_candidatures',[CandidatureController::class,'myCandidatures']);
-        Route::get('/info/{jobId}',[CandidatureController::class,'info']);
+        Route::post('/',[CandidatureController::class,'store'])->name('candidature.store');
+        Route::get('/my_candidatures',[CandidatureController::class,'myCandidatures'])->name('candidature.myCandidatures');
+        Route::get('/info/{jobId}',[CandidatureController::class,'info'])->name('candidature.info');
 
 
         //CandidatureStatus - routes
         Route::group(['prefix'=>'/status'],function(){
-            Route::get('/',[CandidatureStatusController::class,'getAll']);
-            Route::post('/',[CandidatureStatusController::class,'update']);
+            Route::get('/',[CandidatureStatusController::class,'getAll'])->name('candidatureStatus.store');
+            Route::post('/',[CandidatureStatusController::class,'update'])->name('candidatureStatus.update');
 
         });
 
 
         //CandidatureHistory - routes
         Route::group(['prefix'=>'/history'],function(){
-            Route::get('/{jobId}',[CandidatureHistoryController::class,'getHistory']);
+            Route::get('/{jobId}',[CandidatureHistoryController::class,'getHistory'])->name('candidatureHistory.getHistory');
         });
     });
 

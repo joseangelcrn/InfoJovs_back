ca5dad31501bdd3e1e6cd7ca18cab82b36d56f92
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-06-02 13:04:07 +0200

    test_recruiter_change_status_of_candidature_and_generate_an_history_entry

--- tests/Feature/ChangeCandidatureStatusGenerateStatusHistoryAsRecruiterTest.php
+++ tests/Feature/ChangeCandidatureStatusGenerateStatusHistoryAsRecruiterTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Libs\QuestionHelper;
 use App\Models\Candidature;
 use App\Models\CandidatureHistory;
+use App\Models\CandidatureStatus;
 use App\Models\Job;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -53,6 +54,41 @@ class ChangeCandidatureStatusGenerateStatusHistoryAsRecruiterTest extends Passpo
         $this->assertTrue($historyDb->origin_status_id === 1);
         $this->assertTrue($historyDb->destiny_status_id === 1);
 
+
+    }
+
+    public function test_recruiter_change_status_of_candidature_and_generate_an_history_entry(){
+
+        parent::setRole('Recruiter');
+
+        $job = Job::factory()->createOne();
+        $job->recruiter_id = $this->user->id;
+        $job->save();
+
+        $employee = User::factory()->createOne();
+        $employee->assignRole('Employee');
+
+        $candidature =  $employee->candidatures()->create([
+            'job_id'=>$job->id,
+            'questions'=>json_encode(QuestionHelper::createFakeStructureWithResponses())
+        ]);
+
+        $newStatusId = CandidatureStatus::where('id','!=',1)->inRandomOrder()->first()->id;
+
+        $params = [
+            'candidature_ids'=>[$candidature->id],
+            'status_id'=>$newStatusId,
+        ];
+
+        $response = $this->post(route('candidatureStatus.update'),$params);
+        $response->assertStatus(200);
+
+        $candidatureDb = Candidature::with('history')->find($candidature->id);
+        $this->assertTrue($candidatureDb->status_id === $newStatusId);
+
+        $lastHistory = $candidatureDb->history->sortByDesc('created_at')->first();
+
+        $this->assertTrue($lastHistory->destiny_status_id === $newStatusId);
 
     }
 }
